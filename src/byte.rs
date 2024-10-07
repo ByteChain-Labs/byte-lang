22359125177b2678f792d2964b4085f92fa76782
ByteChain-Labs <bytechain.blockchain@gmail.com>
2024-10-07 13:49:58 +0100

    Update byte.rs

--- src/byte.rs
+++ src/byte.rs
@@ -1,1 +1,23 @@
+use std::env;
+use std::fs;
+use std::io{self, Write};
 
+struct Byte;
+
+impl Byte {
+  fn new() -> Self {
+    Byte
+  }
+
+  fn run_file() {
+    println!('')
+  }
+
+  fn run_prompt() {
+    
+  }
+
+  fn run() {
+    
+  }
+}
