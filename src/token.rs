5c44a60f2f37798158e3a1cfd712921fa0106afd
Nifemi Bosun <144700801+nifemibosun@users.noreply.github.com>
2024-10-07 18:13:12 +0100

    Update token.rs

--- src/token.rs
+++ src/token.rs
@@ -1,1 +1,22 @@
+#[derive(Debug)]
+struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    literal: Option<Box<dyn std::any::Any>>,
+    line: usize,
+}
 
+impl Token {
+    fn new(token_type: TokenType, lexeme: String, literal: Option<Box<dyn std::any::Any>>, line: usize) -> Token {
+        Token {
+            token_type,
+            lexeme,
+            literal,
+            line,
+        }
+    }
+
+    fn to_string(&self) -> String {
+        format!("{:?} {} {:?}", self.token_type, self.lexeme, self.literal.as_ref().map(|l| l.type_id()))
+    }
+}
