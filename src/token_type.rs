5ecdeaf978d304cbfcd0af44680a4633e38a4ee0
ByteChain-Labs <bytechain.blockchain@gmail.com>
2024-10-07 11:15:04 +0100

    Update token_type.rs

--- src/token_type.rs
+++ src/token_type.rs
@@ -1,1 +1,50 @@
+enum TokenType {
+    // Keywords
+    STRUCT, CONTRACT,
+    PUBLIC, PRIVATE, PROTECTED,
+    MUT, LET, CONST,
+    FUNC, PRINT,
+    IMPORT, RETURN,
+    IF, ELSE, WHILE, FOR,
+    TRUE, FALSE,
 
+    // Literals
+    INT_LITERAL(i64),
+    UINT_LITERAL(u64),
+    FLOAT_LITERAL(f64),
+    STRING_LITERAL(String),
+    BOOL_LITERAL(bool),
+
+    // Identifiers
+    IDENTIFIER(String),
+
+    // Operators
+    PLUS, MINUS,
+    MULTIPLY, DIVIDE,
+    EQUAL, DOUBLE_EQUAL,
+    NOT_EQUAL, MODULUS,
+    GREATER_THAN, LESS_THAN,
+    GREATER_EQUAL, LESS_EQUAL,
+    AND, OR, NOT,
+
+    // Delimiters
+    OPEN_PAREN, CLOSE_PAREN,
+    OPEN_BRACE, CLOSE_BRACE,
+    OPEN_BRACKET, CLOSE_BRACKET,
+    SEMICOLON, COLON, COMMA,
+
+    // Types
+    INT(i64),
+    UINT(u64),
+    FLOAT(f64),
+    STRING(String),
+    BOOL(bool),
+
+    // Comments
+    LINE_COMMENT(String),
+    BLOCK_COMMENT(String),
+
+    // Whitespace
+    WHITESPACE,
+    NEWLINE,
+}
